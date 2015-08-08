c6aba6671384dfc0675cc3a9688d8725ea1973ad
ishotjr <github@ishotjr.com>
2015-08-08 00:33:08 -0400

    :computer: :abc: Add ability to set message via serial (non-persistent)

--- AutoLOL.ino
+++ AutoLOL.ino
@@ -10,7 +10,7 @@
  *
  * -----------------------------------------------------------------------------
  *
- * 0.1 - Initial PoC to test outdoor LED visibility
+ * 0.2 - Add ability to set message via serial (non-persistent)
  *
  * -----------------------------------------------------------------------------
  *
@@ -21,18 +21,86 @@
 #include <Myfont.h>
 
 // string to display - note \0 termination
-unsigned char message[] = "Honk if you can read this - thanks!:)\0";
-int messageLength = 0;
+// TODO #define/const array size?
+unsigned char message[40] = "Honk if you can read this - thanks!:)\0";
+int messageLength = 38;
+unsigned char newMessage[40] = "\0";
+byte newMessageLength = 0;
+boolean newlineReceived = false;
 
 void setup() {
   
   LedSign::Init();
   
-  // this works since Arduino unsigned char size is 1 byte
-  messageLength = sizeof(message);
-  // no need to do this every time during loop...
+  // initialize serial
+  Serial.begin(9600);
+  
+  Serial.println("setup complete");
 }
 
 void loop() {
+
+  Serial.println("begin loop");
+
+  Serial.println(newlineReceived);
+  
+  // update message on newline receipt
+  if (newlineReceived) {
+
+    //strncpy
+    memcpy(message, newMessage, 40);
+    
+    Serial.println("message updated");//String(newMessage)); 
+    
+    newMessage[0] = '\0';
+    newlineReceived = false;
+    
+    messageLength = newMessageLength;
+
+    newMessageLength = 0;
+  }
+
+  // this works since Arduino unsigned char size is 1 byte
+  //messageLength = sizeof(message);
+
+  Serial.println(messageLength);
+  
+  // message must be unsigned char*
   Myfont::Banner(messageLength, message);
+
+  //Serial.println(newlineReceived);
+
+  Serial.println("end loop");
+
+}
+
+void serialEvent() {
+  
+  Serial.println("serialEvent()");
+
+  while (Serial.available()) {
+    
+    char readChar = Serial.read();
+    
+    Serial.println("read:");
+    Serial.println(readChar);
+
+    unsigned char newChar = (unsigned char)readChar;//Serial.read();
+    
+    // TODO: prevent > 40
+    
+    newMessageLength++;
+    newMessage[newMessageLength] = newChar;
+    
+    if (newChar == '\n') {
+      
+      Serial.println("newline received");
+
+      // replace \n with \0
+      newMessage[newMessageLength] = '\0';
+      newlineReceived = true;
+    }
+    
+    
+  }  
 }
