f84304498776b62c094790ebe50fcd7da7adfccd
ishotjr <github@ishotjr.com>
2015-08-09 01:57:05 -0400

    :lock_with_ink_pen: :closed_lock_with_key: Add message persistence via EEPROM

--- AutoLOL.ino
+++ AutoLOL.ino
@@ -22,19 +22,66 @@
 #include <Charliplexing.h>
 #include <Myfont.h>
 
+// for persisting message
+#include <EEPROM.h>
+
 // ATmega328 has 512b EEPROM, but 50 chars is plenty (+1 for \0)
 #define BUFFER_SIZE 51
 
 // TODO: toggle println via #DEBUG - or...serial?
 
+const int EEPROM_ID = 0x42;   // used to identify if valid data in EEPROM
+
+const int ID_ADDR = 0;        // ID value lives here in EEPROM
+const int MESSAGE_ADDR = 1;   // message starts here in EEPROM
+
 // string to display - note that string literal *includes* \0 termination
-unsigned char message[BUFFER_SIZE] = "Honk if you can read this - thanks!:)";
+unsigned char message[BUFFER_SIZE] = "(-_-) -=No message in EEPROM!=- (-_-)";
 byte messageLength = 37;
 unsigned char newMessage[BUFFER_SIZE] = "";
 byte newMessageLength = 0;
 boolean newlineReceived = false;
 
 void setup() {
+  
+  // check for/load valid message data in EEPROM
+  byte id = EEPROM.read(ID_ADDR);
+  if (id == EEPROM_ID) {
+    for (int i = 0; i < BUFFER_SIZE; i++) {
+      newMessage[i] = EEPROM.read(MESSAGE_ADDR + i);
+      
+      // grab \0 terminator once found and stop reading
+      if (newMessage[i] == '\0') {
+        newMessageLength = i;
+        break;
+      }
+    }
+    
+    // in case there wasn't a null terminator due to funky data
+    if (newMessageLength >= BUFFER_SIZE) {
+      // it can't really be > with the above loop though, right?
+      Serial.println("no terminator while reading EEPROM!");
+      Serial.println(newMessageLength);
+      //newMessage[BUFFER_SIZE - 1] = '\0';
+      //newMessageLength = BUFFER_SIZE - 1;
+      
+      // maybe we should really just fail in this case vs. trying to fix?
+      // (default message will be displayed)   
+      Serial.println("message read from EEPROM FAILED!");
+      Serial.println(newMessageLength);
+    } else {
+      // go ahead and use EEPROM data
+
+      Serial.println("message read from EEPROM succeeded!");
+
+      memcpy(message, newMessage, BUFFER_SIZE);
+    
+      messageLength = newMessageLength;
+    
+      Serial.println("message updated");
+    }
+
+  }
   
   // initialize LoL Shield
   LedSign::Init();
@@ -60,6 +107,15 @@ void loop() {
     messageLength = newMessageLength;
     
     Serial.println("message updated");//String(newMessage)); 
+
+    // write EEPROM_ID to indicate valid data, then write message array
+    EEPROM.write(ID_ADDR, EEPROM_ID);
+    
+    for (int i = 0; i < (newMessageLength + 1); i++) {
+      EEPROM.write(MESSAGE_ADDR + i, message[i]);      
+    }
+
+    Serial.println("message written to EEPROM");
     
     // reset for the next update
     newMessage[0] = '\0';
