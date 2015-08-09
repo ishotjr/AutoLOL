d70cc9354e35b2950ca995ed035d8ba96d579fa4
ishotjr <github@ishotjr.com>
2015-08-08 22:25:24 -0400

    :construction: :abc: Refactor message/serial input

--- AutoLOL.ino
+++ AutoLOL.ino
@@ -20,16 +20,21 @@
 #include <Charliplexing.h>
 #include <Myfont.h>
 
-// string to display - note \0 termination
-// TODO #define/const array size?
-unsigned char message[40] = "Honk if you can read this - thanks!:)\0";
-int messageLength = 38;
-unsigned char newMessage[40] = "\0";
+// ATmega328 has 512b EEPROM, but 50 chars is plenty (+1 for \0)
+#define BUFFER_SIZE 51
+
+// TODO: toggle println via #DEBUG - or...serial?
+
+// string to display - note that string literal *includes* \0 termination
+unsigned char message[BUFFER_SIZE] = "Honk if you can read this - thanks!:)";
+byte messageLength = 37;
+unsigned char newMessage[BUFFER_SIZE] = "";
 byte newMessageLength = 0;
 boolean newlineReceived = false;
 
 void setup() {
   
+  // initialize LoL Shield
   LedSign::Init();
   
   // initialize serial
@@ -48,16 +53,17 @@ void loop() {
   if (newlineReceived) {
 
     //strncpy
-    memcpy(message, newMessage, 40);
+    memcpy(message, newMessage, BUFFER_SIZE);
+
+    messageLength = newMessageLength;
     
     Serial.println("message updated");//String(newMessage)); 
     
+    // reset for the next update
     newMessage[0] = '\0';
     newlineReceived = false;
-    
-    messageLength = newMessageLength;
+    newMessageLength = 0;    
 
-    newMessageLength = 0;
   }
 
   // this works since Arduino unsigned char size is 1 byte
@@ -75,32 +81,56 @@ void loop() {
 }
 
 void serialEvent() {
+  // TODO: do we have to worry about overflowing the buffer here?
+  // *seems to be limited to 64 characters anyway based on experimentation?*
+  byte inputLength = 0;
   
   Serial.println("serialEvent()");
 
   while (Serial.available()) {
     
     char readChar = Serial.read();
+    inputLength++;
+    Serial.println(inputLength);
     
     Serial.println("read:");
     Serial.println(readChar);
 
     unsigned char newChar = (unsigned char)readChar;//Serial.read();
     
-    // TODO: prevent > 40
-    
-    newMessageLength++;
-    newMessage[newMessageLength] = newChar;
-    
-    if (newChar == '\n') {
+    // just ignore anything over BUFFER_SIZE for now
+    if (inputLength < BUFFER_SIZE) {
       
-      Serial.println("newline received");
+      // - 1 since 0-index
+      newMessage[inputLength - 1] = newChar;
 
-      // replace \n with \0
-      newMessage[newMessageLength] = '\0';
-      newlineReceived = true;
-    }
+      // terminate the string on newline
+      if (newChar == '\n') {
+        
+        Serial.println("newline received");
+  
+        // replace \n with \0
+        newMessage[inputLength - 1] = '\0';
+        newlineReceived = true;
+        
+        // we don't coun't the \0, so:
+        newMessageLength = inputLength - 1;      
+
+      }
     
+    } else if (inputLength == BUFFER_SIZE) {
+      
+      Serial.println("buffer full");
+      
+      // we're out of space, so pretend it was a newline regardless
+      newMessage[inputLength - 1] = '\0';
+      newlineReceived = true;
+      
+      // new message length is as long as it can be!
+      newMessageLength = BUFFER_SIZE - 1;      
+    } else {
+      Serial.println("ignore since > BUFFER_SIZE"); 
+    }
     
   }  
 }
